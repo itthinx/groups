e6ec9e70fe6ba40b8df9d6be9b738cdf6ccf32cd
blancoleon <antonio@blancoleon>
2013-10-30 16:01:52 +0100

    add posts filter

--- lib/admin/class-groups-admin-posts.php
+++ lib/admin/class-groups-admin-posts.php
@@ -36,6 +36,12 @@ class Groups_Admin_Posts {
 	 * capability.
 	 */
 	public static function admin_init() {
+		global $pagenow;
+		
+		if ( $pagenow == 'edit.php' ) {
+			Groups_UIE::enqueue( 'select' );
+		}
+		
 		add_action( 'restrict_manage_posts', array( __CLASS__, 'restrict_manage_posts' ) );
 		add_filter( 'parse_query', array( __CLASS__, 'parse_query' ) );
 	}
@@ -45,7 +51,7 @@ class Groups_Admin_Posts {
 	 */
 	public static function restrict_manage_posts() {
 
-		global $pagenow;
+		global $pagenow, $wpdb;
 
 		if ( is_admin() ) {
 
@@ -53,13 +59,27 @@ class Groups_Admin_Posts {
 
 				$post_type = isset( $_GET['post_type'] ) ? $_GET['post_type'] : 'post';
 
-				if ( false ) { // @todo check that access restriction is enabled for the $post_type
-
+				$post_types_option = Groups_Options::get_option( Groups_Post_Access::POST_TYPES, array() );
+				
+				if ( !isset( $post_types_option[$post_type]['add_meta_box'] ) || $post_types_option[$post_type]['add_meta_box'] ) {
+				
 					$output = '';
 
-					// @todo print the access restriction filter field for post types that have it enabled
-
-					// @todo for now, let's use as the field name : Groups_Post_Access::POSTMETA_PREFIX . Groups_Post_Access::READ_POST_CAPABILITY
+					$capability_table = _groups_get_tablename( "capability" );
+					
+					// capabilities select
+					$capabilities = $wpdb->get_results( "SELECT * FROM $capability_table ORDER BY capability" );
+					$output .= sprintf(
+							'<select class="select capability" name="%s[]" multiple="multiple" placeholder="%s" data-placeholder="%s">',
+							esc_attr( Groups_Post_Access::POSTMETA_PREFIX . Groups_Post_Access::READ_POST_CAPABILITY ),
+							esc_attr( __( 'Capabilities &hellip;', GROUPS_PLUGIN_DOMAIN ) ) ,
+							esc_attr( __( 'Capabilities &hellip;', GROUPS_PLUGIN_DOMAIN ) )
+					);
+					foreach( $capabilities as $capability ) {
+						$output .= sprintf( '<option value="%s">%s</option>', esc_attr( $capability->capability_id ), wp_filter_nohtml_kses( $capability->capability ) );
+					}
+					$output .= '</select>';
+					$output .= Groups_UIE::render_select( '.select.capability' );
 
 					echo $output;
 				}
@@ -85,16 +105,29 @@ class Groups_Admin_Posts {
 
 				$post_type = isset( $_GET['post_type'] ) ? $_GET['post_type'] : 'post';
 
-				if ( false ) { // @todo check that access restriction is enabled for the $post_type
-
+				$post_types_option = Groups_Options::get_option( Groups_Post_Access::POST_TYPES, array() );
+				
+				if ( !isset( $post_types_option[$post_type]['add_meta_box'] ) || $post_types_option[$post_type]['add_meta_box'] ) {
+				
 					if ( !empty( $_GET[Groups_Post_Access::POSTMETA_PREFIX . Groups_Post_Access::READ_POST_CAPABILITY] ) ) {
 
-						$capability_id = Groups_Utility::id( $_GET[Groups_Post_Access::POSTMETA_PREFIX . Groups_Post_Access::READ_POST_CAPABILITY] );
-
+						//$capability_id = Groups_Utility::id( $_GET[Groups_Post_Access::POSTMETA_PREFIX . Groups_Post_Access::READ_POST_CAPABILITY] );
+						$capability_id = $_GET[Groups_Post_Access::POSTMETA_PREFIX . Groups_Post_Access::READ_POST_CAPABILITY];
+						
+						ob_start();
+						var_dump($capability_id[0]);
+						$result = ob_get_clean();
+						error_log($result);
+						
 						// modify the $query to take the access restriction filter into account
 
-						$query->query_vars['meta_key'] = Groups_Post_Access::POSTMETA_PREFIX . Groups_Post_Access::READ_POST_CAPABILITY;
-						$query->query_vars['meta_value'] = $capability_id;
+						//$query->query_vars['meta_key'] = Groups_Post_Access::POSTMETA_PREFIX . Groups_Post_Access::READ_POST_CAPABILITY;
+						//$query->query_vars['meta_value'] = $capability_id;
+						$query->query_vars['meta_query'] = array ( array (
+								'key' => Groups_Post_Access::POSTMETA_PREFIX . Groups_Post_Access::READ_POST_CAPABILITY,
+								'value' => array(intval($capability_id[0])),
+								'compare' => 'IN'
+								));
 					}
 				}
 			}
