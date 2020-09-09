5994d051fdef5faacbcda45cdc93ec04c11e2ea0
itthinx <itthinx@gmail.com>
2020-09-10 00:16:41 +0200

    copyright updated

--- groups.php
+++ groups.php
@@ -2,7 +2,7 @@
 /**
  * groups.php
  *
- * Copyright (c) 2011-2019 "kento" Karim Rahimpur www.itthinx.com
+ * Copyright (c) 2011-2020 "kento" Karim Rahimpur www.itthinx.com
  *
  * This code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt.
