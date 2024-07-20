759f01de51a464457fd6738f816aa0ad86622eb1
itthinx <itthinx@itthinx.com>
2024-07-20 17:39:48 +0200

    unified store

--- lib/blocks/build/index.asset.php
+++ lib/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'bf8ad369654bbc54860a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '638078029bd27e96b93f');
