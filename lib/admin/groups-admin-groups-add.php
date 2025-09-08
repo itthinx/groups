48a84a362f2f1bb6798ec1a07b580b0e4281c8fd
itthinx <itthinx@itthinx.com>
2025-09-09 00:05:57 +0200

    fix performance on group tree, resolves issue 158

--- lib/admin/groups-admin-groups-add.php
+++ lib/admin/groups-admin-groups-add.php
@@ -50,8 +50,8 @@ function groups_admin_groups_add() {
 		'<option value="" %s>--</option>',
 		empty( $parent_id ) ? 'selected="selected"' : ''
 	);
-	$tree = Groups_Utility::get_group_tree();
-	Groups_Utility::render_group_tree_options( $tree, $parent_select, 0, array( $parent_id ) );
+	$tree = Groups_Utility::get_tree();
+	Groups_Utility::render_tree_options( $tree, $parent_select, 0, array( $parent_id ) );
 	$parent_select .= '</select>';
 
 	$output .= '<div class="manage-groups wrap">';
--- lib/core/class-groups-utility.php
+++ lib/core/class-groups-utility.php
@@ -29,6 +29,24 @@ if ( !defined( 'ABSPATH' ) ) {
 class Groups_Utility {
 
 	/**
+	 * Transient expiration, 1 minute.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @var integer
+	 */
+	const TREE_TRANSIENT_EXPIRATION = 60;
+
+	/**
+	 * Data cache during requests to avoid expensive operations.
+	 *
+	 * @since 3.7.0
+	 *
+	 * @var array
+	 */
+	private static $cache = array();
+
+	/**
 	 * Checks an id (0 is accepted => anonymous).
 	 *
 	 * @param string|int $id
@@ -48,6 +66,7 @@ class Groups_Utility {
 
 	/**
 	 * Returns an array of blog_ids for current blogs.
+	 *
 	 * @return array of int with blog ids
 	 */
 	public static function get_blogs() {
@@ -69,9 +88,170 @@ class Groups_Utility {
 		return $result;
 	}
 
+	/**
+	 * Object sort helper, sort by name property.
+	 *
+	 * @param object $o1
+	 * @param object $o2
+	 *
+	 * @return int
+	 */
+	private static function namesort( $o1, $o2 ) {
+		return strcmp( $o1->name, $o2->name );
+	}
+
+	/**
+	 * Provide the groups tree.
+	 *
+	 * @since 3.7.0
+	 *
+	 * @access private
+	 *
+	 * @return object[]
+	 */
+	public static function get_tree() {
+
+		global $wpdb;
+
+		if ( isset( self::$cache['tree'] ) ) {
+			return self::$cache['tree'];
+		}
+
+		$tree = get_transient( 'groups_utility_tree' );
+		if ( is_array( $tree ) ) {
+			self::$cache['tree'] = $tree;
+			return $tree;
+		}
+
+		$group_table = _groups_get_tablename( 'group' );
+		// Note that ORDER BY name is not necessary as it is done after obtaining the rows during processing below
+		$map = $wpdb->get_results( "SELECT group_id, parent_id, name FROM $group_table" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+
+		$prune = array();
+
+		$objects = array();
+		foreach ( $map as $entry ) {
+			if ( $entry->parent_id !== null ) {
+				if ( !array_key_exists( $entry->parent_id, $objects ) ) {
+					// create parent entry
+					$p = new stdClass();
+					$p->name = null; // completed below
+					$p->group_id = $entry->parent_id;
+					$p->children = array();
+					$objects[$p->group_id] = $p;
+				}
+			}
+
+			if ( !array_key_exists( $entry->group_id, $objects ) ) {
+				$o = new stdClass();
+				$o->name = $entry->name;
+				$o->group_id = $entry->group_id;
+				$o->children = array();
+				$objects[$o->group_id] = $o;
+				if ( $entry->parent_id !== null ) {
+					$objects[$entry->parent_id]->children[] = $o;
+					uasort( $objects[$entry->parent_id]->children, array( __CLASS__, 'namesort' ) );
+				}
+			} else {
+				// complete name from parent entry created
+				if ( $objects[$entry->group_id]->name === null ) {
+					$objects[$entry->group_id]-> name = $entry->name;
+				}
+				if ( $entry->parent_id !== null ) {
+					$objects[$entry->parent_id]->children[] = $objects[$entry->group_id];
+					uasort( $objects[$entry->parent_id]->children, array( __CLASS__, 'namesort' ) );
+				}
+			}
+
+			if ( $entry->parent_id !== null ) {
+				$prune[] = $entry->group_id;
+			}
+		}
+
+		foreach ( $prune as $group_id ) {
+			unset( $objects[$group_id] );
+		}
+
+		uasort( $objects, array( __CLASS__, 'namesort' ) );
+
+		set_transient( 'groups_utility_tree', $objects, self::TREE_TRANSIENT_EXPIRATION );
+
+		self::$cache['tree'] = $objects;
+
+		return $objects;
+	}
+
+	/**
+	 * Render options from tree for select.
+	 *
+	 * @since 3.7.0
+	 *
+	 * @param array $tree
+	 * @param string $output
+	 * @param int $level
+	 */
+	public static function render_tree_options( &$tree, &$output, $level = 0, $selected = array() ) {
+		foreach( $tree as $group_id => $object ) {
+			$output .= sprintf(
+				'<option class="node" value="%d" %s>',
+				esc_attr( $group_id ),
+				in_array( $group_id, $selected ) ? 'selected' : ''
+			);
+			// If specific filtering is done on the group data, we might need to pass it through this call and use the name of the $group object instead:
+			// $group = Groups_Group::read( $group_id );
+			if ( $level > 0 ) {
+				$output .= str_repeat( "&nbsp;", $level ) . "&llcorner;";
+			}
+			$output .= $object->name ? stripslashes( wp_filter_nohtml_kses( $object->name ) ) : '';
+			$output .= '</option>';
+			if ( !empty( $object->children ) ) {
+				self::render_tree_options( $object->children, $output, $level + 1, $selected );
+			}
+		}
+	}
+
+	/**
+	 * Render the group tree to the $output parameter passed by reference.
+	 *
+	 * @since 3.7.0
+	 *
+	 * @param array $tree
+	 * @param string $output
+	 */
+	public static function render_tree( &$tree, &$output, $linked = false ) {
+		$output .= '<ul class="groups-tree">';
+		foreach( $tree as $group_id => $object ) {
+			$output .= '<li class="groups-tree-node">';
+			// If specific filtering is done on the group data, we might need to pass it through this call and use the name of the $group object instead:
+			// $group = Groups_Group::read( $group_id );
+			if ( $linked ) {
+				$edit_url = add_query_arg(
+					array(
+						'group_id' => intval( $object->group_id ),
+						'action' => 'edit'
+					),
+					get_admin_url( null, 'admin.php?page=groups-admin' )
+				);
+				$output .= sprintf( '<a href="%s">', $edit_url );
+			}
+			$output .= $object->name ? stripslashes( wp_filter_nohtml_kses( $object->name ) ) : '';
+			if ( $linked ) {
+				$output .= '</a>';
+			}
+			if ( !empty( $object->children ) ) {
+				self::render_tree( $object->children, $output, $linked );
+			}
+			$output .= '</li>';
+		}
+		$output .= '</ul>';
+	}
 
 	/**
 	 * Get the tree hierarchy of groups.
+	 *
+	 * This method is inefficent. Internal uses replaced by get_tree() as of 3.7.0.
+	 *
+	 * @deprecated since 3.7.0
 	 *
 	 * @param array $tree
 	 *
@@ -90,6 +270,7 @@ class Groups_Utility {
 				}
 			}
 			self::get_group_tree( $tree );
+			self::$cache['tree'] = $tree;
 		} else {
 			foreach( $tree as $group_id => $nodes ) {
 				$children = $wpdb->get_results( $wpdb->prepare(
@@ -107,6 +288,8 @@ class Groups_Utility {
 
 	/**
 	 * Render options from tree for select.
+	 *
+	 * @deprecated since 3.7.0. Internal uses replaced by get_tree_options() as of 3.7.0.
 	 *
 	 * @since 2.19.0
 	 *
@@ -137,6 +320,8 @@ class Groups_Utility {
 
 	/**
 	 * Render the group tree to the $output parameter passed by reference.
+	 *
+	 * @deprecated since 3.7.0. Internal uses replaced by render_tree() as of 3.7.0.
 	 *
 	 * @param array $tree
 	 * @param string $output
