ffb43a04e760bbf6856b288518418c6ce9fdb8f2
itthinx <itthinx@itthinx.com>
2026-01-05 19:14:51 +0100

    blocks can be transformed into each other

--- lib/blocks/build/index.asset.php
+++ lib/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '9b6ac432730972036edf');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '8a51f3399082aeb7736c');
