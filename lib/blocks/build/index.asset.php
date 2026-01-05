80fd190cddff60c270b476ad993cdc2929920f20
itthinx <itthinx@itthinx.com>
2026-01-05 19:02:56 +0100

    nested member blocks indentation

--- lib/blocks/build/index.asset.php
+++ lib/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'ea19d2b782ba39c31633');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '9b6ac432730972036edf');
