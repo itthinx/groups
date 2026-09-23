0319a91c9b989b42c2062ef6ac81731fb005ea03
itthinx <itthinx@itthinx.com>
2026-09-23 14:00:02 +0200

    version 4.8.0

--- groups.php
+++ groups.php
@@ -21,11 +21,11 @@
  * Plugin Name: Groups
  * Plugin URI: https://www.itthinx.com/plugins/groups
  * Description: Groups provides group-based user membership management, group-based capabilities and content access control.
- * Version: 4.7.1
+ * Version: 4.8.0
  * Requires at least: 6.7
  * Requires PHP: 7.4
  * WC requires at least: 10.0
- * WC tested up to: 11.1
+ * WC tested up to: 11.2
  * Author: itthinx
  * Author URI: https://www.itthinx.com
  * Donate-Link: https://www.itthinx.com/shop/
@@ -36,7 +36,7 @@
 if ( !defined( 'ABSPATH' ) ) {
 	exit;
 }
-define( 'GROUPS_CORE_VERSION', '4.7.1' );
+define( 'GROUPS_CORE_VERSION', '4.8.0' );
 define( 'GROUPS_FILE', __FILE__ );
 if ( !defined( 'GROUPS_CORE_DIR' ) ) {
 	define( 'GROUPS_CORE_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
